338ec335db0351c347c2bae2554cfccafe1a04ed
Benjamin Phillips <bphillips3@sae.au>
2018-06-25 14:55:39 +1000

    flight is working better. rotates side to side which is functional

--- MusicProj/Assets/Scripts/PlayerFlight.cs
+++ MusicProj/Assets/Scripts/PlayerFlight.cs
@@ -14,17 +14,27 @@ public class PlayerFlight : MonoBehaviour
     public Vector3 offset = new Vector3(0, 0, 2),
         lookdir;
 
-    public float baseSpeed;
+    public float baseSpeed,
+        yaw;
+
+    public bool start,
+        camFollow,
+        endGame;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        start = false;
+        camFollow = false;
     }
-    
+
     void FixedUpdate()
     {
         //sets the players velocity so it moves forward
-        rb.velocity = transform.forward * baseSpeed;
+        if (start == true)
+        {
+            rb.velocity = transform.forward * baseSpeed;
+        }
         PlayerInput();
         BirdMovement();
     }
@@ -37,19 +47,38 @@ public class PlayerFlight : MonoBehaviour
 
         transform.Translate(x, 0, 0);
         transform.Translate(0, y, 0);
+
+        if (x > 0 || y > 0)
+        {
+            start = true;
+            camFollow = true;
+        }
     }
 
     //Moves the player bird toward the point of flight
     public void BirdMovement()
     {
+        //find the yaw
+        yaw = Input.GetAxis("Horizontal") * Time.deltaTime;
+        yaw = -yaw * 5000;
+
         //finds the rotation needed to look at
         var blook = Quaternion.LookRotation(gameObject.transform.position - bird.transform.position);
         var clook = Quaternion.LookRotation(bird.transform.position - cam.transform.position);
 
         //moves the objects at a delay and rotates them accordingly
         bird.transform.position = Vector3.Lerp(bird.transform.position, gameObject.transform.position - offset, Time.deltaTime);
-        cam.transform.position = Vector3.Lerp(cam.transform.position, bird.transform.position - (offset * 0.5f), Time.deltaTime);
         bird.transform.LookAt(gameObject.transform.position);
-        cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation, clook, Time.deltaTime);
+        bird.transform.rotation = Quaternion.Lerp(bird.transform.rotation, Quaternion.Euler(new Vector3(bird.transform.rotation.x, bird.transform.rotation.y, yaw)), 20 * Time.deltaTime);
+
+        if(camFollow == true)
+        {
+            cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation, clook, Time.deltaTime);
+            cam.transform.position = Vector3.Lerp(cam.transform.position, bird.transform.position - (offset * 0.5f), Time.deltaTime);
+        }
+        else
+        {
+            cam.transform.LookAt(bird.transform.position);
+        }
     }
 }
