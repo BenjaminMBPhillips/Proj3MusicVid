63abbaa82f309c81f4cd5182ed601328beed24a9
SPOOKYBOY\benn7 <benn7495@gmail.com>
2018-07-04 11:49:59 +1000

    YA BOII GOT THE BIRD SO FREAKING CLOSE TO WORKING

--- MusicProj/Assets/Scripts/AttemptFlight.cs
+++ MusicProj/Assets/Scripts/AttemptFlight.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class AttemptFlight : MonoBehaviour
 {
-    public GameObject birdModel;
+    public GameObject birdModel,
+        modelContainer;
 
     public float smoothRot;
 
@@ -14,7 +15,7 @@ public class AttemptFlight : MonoBehaviour
 
     public float rollrot;
 
-    public Rigidbody rb; 
+    public Rigidbody rb;
     // Use this for initialization
     void Start()
     {
@@ -44,7 +45,7 @@ public class AttemptFlight : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, CheckTerrainHeight + 1, transform.position.z);
         }
-    } 
+    }
     #endregion
 
     void ModelMoveAndRotate()
@@ -57,14 +58,42 @@ public class AttemptFlight : MonoBehaviour
 
         //moves the objects at a delay and rotates them accordingly
         birdModel.transform.position = Vector3.Lerp(birdModel.transform.position, gameObject.transform.position, Time.deltaTime);
-        //
 
-        //birdModel.transform.Rotate(0.0f, 0.0f, -Input.GetAxis("Horizontal"));
+        birdModel.transform.LookAt(transform.position, transform.up);
 
-        //birdModel.transform.rotation = Quaternion.RotateTowards(birdModel.transform.rotation, Quaternion.Euler(new Vector3(pitchrot, 0, rollrot)), smoothRot * Time.deltaTime);
-        birdModel.transform.LookAt(transform.position);
+        modelContainer.transform.Rotate(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+
+        #region Attempts
+        /*Vector3 target = transform.position - birdModel.transform.position;
+       float step = speed * Time.deltaTime;
+       var ZForce = -Input.GetAxis("Horizontal") * 100;
+
+       birdModel.transform.Rotate(Vector3.forward, Time.deltaTime * ZForce);
+
+
+       Vector3 newTarg = target + new Vector3(target.x, target.y, ZForce);
+       Vector3 newdir = Vector3.RotateTowards(birdModel.transform.forward, newTarg, step, 0.0f);
+       Vector3 zRot = new Vector3(birdModel.transform.rotation.x, birdModel.transform.rotation.y, -Input.GetAxis("Horizontal"));*/
+
+        /*Vector3 target = transform.position - birdModel.transform.position;
+        var yRot = target.y;
+        var xRot = target.x;
+        var zRot = -Input.GetAxis("Horizontal");
+        Vector3 newDir = new Vector3(yRot, xRot, zRot);
+        float step = speed * Time.deltaTime;
+        Vector3 lookDir = Vector3.RotateTowards(birdModel.transform.forward, newDir, step, 0.0f);
+        birdModel.transform.rotation = Quaternion.Euler(lookDir);*/
+
+
+        // birdModel.transform.Rotate(0.0f, 0.0f, -Input.GetAxis("Horizontal"));
+
+        // birdModel.transform.rotation = Vector3.RotateTowards(birdModel.transform.rotation, Quaternion.Euler(new Vector3(transform.position.x, transform.position.y, -Input.GetAxis("Horizontal"))));
+
+        //birdModel.transform.rotation = Quaternion.RotateTowards(birdModel.transform.rotation, Quaternion.Euler(new Vector3(pitchrot, 0, rollrot)), smoothRot * Time.deltaTime); 
+        #endregion
     }
 
+    #region Camera
     void CamFollow()
     {
         Vector3 camMove = birdModel.transform.position - transform.forward * 10 + Vector3.up * 5;
@@ -72,4 +101,7 @@ public class AttemptFlight : MonoBehaviour
         Camera.main.transform.position = Camera.main.transform.position * bias + camMove * (1.0f - bias);
         Camera.main.transform.LookAt(birdModel.transform.position + transform.forward * 30);
     }
+    #endregion
+
+   
 }
---
+++ MusicProj/Assets/Scripts/RotClamp.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotClamp : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClampRotation(-75, 75, 0);
+    }
+
+    void ClampRotation(float minAngle, float maxAngle, float clampAroundAngle = 0)
+    {
+        //clampAroundAngle is the angle you want the clamp to originate from
+        //For example a value of 90, with a min=-45 and max=45, will let the angle go 45 degrees away from 90
+
+        //Adjust to make 0 be right side up
+        clampAroundAngle += 180;
+
+        //Get the angle of the z axis and rotate it up side down
+        float z = transform.rotation.eulerAngles.z - clampAroundAngle;
+
+        z = WrapAngle(z);
+
+        //Move range to [-180, 180]
+        z -= 180;
+
+        //Clamp to desired range
+        z = Mathf.Clamp(z, minAngle, maxAngle);
+
+        //Move range back to [0, 360]
+        z += 180;
+
+        //Set the angle back to the transform and rotate it back to right side up
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, z + clampAroundAngle);
+    }
+
+    //Make sure angle is within 0,360 range
+    float WrapAngle(float angle)
+    {
+        //If its negative rotate until its positive
+        while (angle < 0)
+            angle += 360;
+
+        //If its to positive rotate until within range
+        return Mathf.Repeat(angle, 360);
+    }
+}
