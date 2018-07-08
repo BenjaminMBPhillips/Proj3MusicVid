668d52540a875a10419ff50f0aaeedcad926fb66
SPOOKYBOY\benn7 <benn7495@gmail.com>
2018-07-08 13:37:16 +1000

    added lighting changes during play

---
+++ MusicProj/Assets/Scripts/Lighting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour
+{
+    public Light lit;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lit.color = Color.Lerp(lit.color, Color.black, 0.01f * Time.deltaTime);
+    }
+}
